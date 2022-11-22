b1808381afa549b54b63d47e3c89ff51a5f9f26b
shivamgopchade <83418111+shivamgopchade@users.noreply.github.com>
2022-11-22 19:22:52 +0530

    changed title

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -45,7 +45,7 @@ function HomeScreen() {
   return (
     <div>
       <Helmet>
-        <title>Shopify</title>
+        <title>DealKart</title>
       </Helmet>
       <h1>Featured Products</h1>
       <div className="products">
